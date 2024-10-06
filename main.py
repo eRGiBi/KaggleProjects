ef3c1251d4432ab99704c4df2a5727152ed30257
eRGiBi <xx4qwexx@gmail.com>
2024-10-06 23:35:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ if __name__ == '__main__':
 
     # TODO: ArgParser
 
-    # HousePricesRegression(algorithm="NN", tune=False).run()
+    HousePricesRegression(algorithm="NN", tune=False).run()
     # HousePricesRegression(algorithm="yggdf", tune=True, submit=True).run()
-    HousePricesRegression(algorithm="ensemble", tune=True).run()
+    # HousePricesRegression(algorithm="ensemble", tune=True).run()
+
+
