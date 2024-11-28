76744d9709574ac92f423e40773245ef230048b5
eRGiBi <xx4qwexx@gmail.com>
2024-11-28 22:10:18 +0100

    Trials

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="NN", submit=False, SEED=SEED)
     # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="yggdf", tune=False, submit=False, SEED=SEED)
     # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="sklearn_rf", tune=False, submit=False, SEED=SEED)
-    HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="grb", submit=False, SEED=SEED)
+    # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="grb", submit=False, SEED=SEED)
 
-    # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="ensemble", submit=False, SEED=SEED)
+    HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="ensemble", submit=False, SEED=SEED)
