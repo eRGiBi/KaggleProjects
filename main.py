dbc3b35426c70062067c38ed6519d53427fb3d53
eRGiBi <xx4qwexx@gmail.com>
2024-12-12 23:56:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from HousePrices.HousePrices import HousePricesRegressionEnv
 
 if __name__ == '__main__':
-
-    SEED = 476
-
     # TODO: ArgParser
+
+    SEED = 1476
 
     # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="NN", submit=False, SEED=SEED)
     # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="yggdf", tune=False, submit=False, SEED=SEED)
     # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="sklearn_rf", tune=False, submit=False, SEED=SEED)
     # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="grb", submit=False, SEED=SEED)
 
-    HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="ensemble", submit=False, SEED=SEED)
+    HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="ensemble", submit=True, SEED=SEED)
