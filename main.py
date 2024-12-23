e87369e65ad8c92a96abee991be4b9945462a061
eRGiBi <xx4qwexx@gmail.com>
2024-12-23 01:13:36 +0100

    Ridge regression added

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ if __name__ == '__main__':
     # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="NN", submit=False, SEED=SEED)
     # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="yggdf", tune=False, submit=False, SEED=SEED)
     # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="sklearn_rf", tune=False, submit=False, SEED=SEED)
-    # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="grb", submit=False, SEED=SEED)
+    # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="ridge", submit=False, SEED=SEED)
 
-    HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="ensemble", submit=False, SEED=SEED)
+    # HousePricesRegressionEnv(SEED=SEED, visualize=False).run_regression(algorithm="grb",
+    #                                                                     tune=True,
+    #                                                                     submit=False,
+    #                                                                     SEED=SEED)
+
+    HousePricesRegressionEnv(SEED=SEED, visualize=False).run_regression(algorithm="ensemble", submit=True, SEED=SEED)
