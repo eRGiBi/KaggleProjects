c09f5809945712f5ed8d4d78c7eaba61aa2a0996
eRGiBi <xx4qwexx@gmail.com>
2025-02-12 23:26:12 +0100

    FL continued

--- ISIC2024_SkinCancerDetection/CustomFit.py
+++ ISIC2024_SkinCancerDetection/CustomFit.py
@@ -4,3 +4,21 @@ def fit():
 
     # Focal loss
     # 
+
+    device = "cuda"
+    model.to(device)
+
+    gradient_accumulation_steps = 2
+
+    for index, batch in enumerate(training_dataloader):
+        inputs, targets = batch
+        inputs = inputs.to(device)
+        targets = targets.to(device)
+        outputs = model(inputs)
+        loss = loss_function(outputs, targets)
+        loss = loss / gradient_accumulation_steps
+        loss.backward()
+        if (index + 1) % gradient_accumulation_steps == 0:
+            optimizer.step()
+            scheduler.step()
+            optimizer.zero_grad()
