60fceb3144355c293c7d707d2d40da165a5981e0
eRGiBi <xx4qwexx@gmail.com>
2025-06-29 01:12:08 +0200

    Refactoring

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from HousePrices.HousePrices import HousePricesRegressionEnv
+from PolymerPrediction.main import PolymerPredictionEnv
 
 if __name__ == '__main__':
     # TODO: ArgParser
 
     SEED = 476
 
-    # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="NN", submit=False, SEED=SEED)
-    # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="yggdf", tune=False, submit=False, SEED=SEED)
-    # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="sklearn_rf", tune=False, submit=False, SEED=SEED)
-    # HousePricesRegressionEnv(SEED=SEED, visualize=False).run_regression(algorithm="ridge", submit=False, SEED=SEED)
+    algorithm = [
+        "NN", "yggdf", "sklearn_rf", "ridge", "grb", "ensemble"
+    ]
 
-    # HousePricesRegressionEnv(SEED=SEED).run_regression(algorithm="grb",
-    #                                                    tune=True,
-    #                                                    submit=False,
-    #                                                    SEED=SEED)
-    #
-    HousePricesRegressionEnv(SEED=SEED, visualize=False).run_regression(algorithm="ensemble", submit=True, SEED=SEED)
+    HousePricesRegressionEnv(
+        SEED=SEED, visualize=False).run_regression(
+            algorithm="ensemble", submit=True, SEED=SEED
+    )
+
+    PolymerPredictionEnv(seed).run_experiment()
